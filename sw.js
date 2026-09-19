78f1883054f85222049539d94f94ff2da7551c7c
Zhi Dong Dong <49508823+ZhiDong02134@users.noreply.github.com>
2026-09-19 01:41:27 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
   Bump CACHE when older cache namespaces must be evicted.
 */
 const CACHE_PREFIX = 'twhk-2026-';
-const CACHE = `${CACHE_PREFIX}v13`;
+const CACHE = `${CACHE_PREFIX}v14`;
 const SHELL = [
   './index.html',
   './manifest.webmanifest',
